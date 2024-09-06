923ca6bce6916e16fc581c6b7242aba307285196
Giuseppe Cifarelli <cifarelli1995@gmail.com>
2024-09-06 17:52:59 +0200

    fetchUser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+function UserList() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        if(!response.ok) {
+          throw new Error('Errore nella rete');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setUsers(data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setIsLoading(false);
+      })
+  }, []);
+
+  if(isLoading) {
+    return <p>Caricamento in corso...</p>
+  }
+
+  if(error) {
+    return <p>Errore: {error}</p>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista Utenti:</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name} ({user.mail})</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
+}
+function App() {
+  return <UserList></UserList>
 }
 
 export default App;
